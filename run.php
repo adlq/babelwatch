dd197831664897f0794d3aab41d6fb5e4fbf2fa2
Nghia D <tnghia.duong@gmail.com>
2013-07-29 15:57:14 +0200

    Clean update

--- run.php
+++ run.php
@@ -8,7 +8,7 @@ foreach ($GLOBALS['conf']['repo'] as $repoName => $repoInfo)
 		if (array_key_exists('repoPath', $repoInfo))
 		{
 			chdir($repoInfo['repoPath']);
-			exec('hg incoming --bundle incoming.hg && hg pull --update incoming.hg && cd ' . __DIR__ . ' && php build.php ' . $repoName);
+			exec('hg incoming --bundle incoming.hg && hg pull incoming.hg && hg update --clean && cd ' . __DIR__ . ' && php build.php ' . $repoName);
 		}
 	}
 }
